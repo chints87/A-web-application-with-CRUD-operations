319723ab39cdcbdfdf55a7a78c77e4ef2c4c7695
Chintan Gandhi <cgandhi4@gmail.com>
2018-02-14 16:28:50 +0400

    Added templates

--- application.py
+++ application.py
@@ -3,32 +3,33 @@ app = Flask(__name__)
 
 @app.route('/login') 
 def login(): 
-    return ('This is the login page')
+    # return ('This is the login page')
+    return render_template('login.html')
 
 @app.route('/')
 @app.route('/catalog')
 def showCatalog():
-    return ('This shows all the categories and latest added items')
+    return render_template('catalog.html')
 
 @app.route('/catalog/additem')
 def newItem():
-    return ('This is a form to enter new item')
+    return render_template('newItem.html')
 
 @app.route('/catalog/category/items')
 def showItems(): 
-    return ('Displays all available items in a specific category')
+    return render_template('categoryitems.html')
 
 @app.route('/catalog/category/item_name')
 def showDetails(): 
-    return('Displays the description of that item in the specfic category')
+    return render_template('itemdetail.html')
 
 @app.route('/catalog/category/item_name/edit')
 def editItem(): 
-    return ('Edit item')
+    return render_template('edititem.html')
 
 @app.route('/catalog/category/item_name/delete')
 def deleteItem(): 
-    return ('Delete item')
+    return render_template('deleteitem.html')
 
 if __name__ == '__main__': 
     app.debug = True
